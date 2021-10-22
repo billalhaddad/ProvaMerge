402bd5077eebaa4d69e05122addba5571d12fb81
Billal Haddad <BILLAL.HADDAD@local.ittcesena.it>
2021-10-22 09:10:18 +0200

    modificaa del message box

--- TestGit/MainWindow.xaml.cs
+++ TestGit/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace TestGit
         public MainWindow()
         {
             InitializeComponent();
-            MessageBox.Show("TE");
+            MessageBox.Show("prova");
         }
     }
 }
